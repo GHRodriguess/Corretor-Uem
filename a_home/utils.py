ae9fcc44cdd1007fd8bf04ea5275e889e809b1c8
GHRodriguess <gabri.henri20088@gmail.com>
2025-01-22 18:50:59 -0300

    mais um ajuste

--- a_home/utils.py
+++ a_home/utils.py
@@ -6,10 +6,9 @@ def get_vestibulares() -> dict:
     #base_dir = os.path.join("VESTIBULARES") 
     #PARA PRODUÇÃO
     base_dir = os.path.join("Corretor-Uem","VESTIBULARES") 
-          
     vestibulares = [vestibular for vestibular in sorted(os.listdir(base_dir), reverse=True)]        
     imgs = [os.path.join("media", vestibular, "img.jpg") for vestibular in vestibulares]    
-    status = [os.path.exists(os.path.join(base_dir, vestibular, "GABARITOS")) and len(os.listdir(os.path.join("VESTIBULARES", vestibular, "GABARITOS"))) >= 1 for vestibular in vestibulares]
+    status = [os.path.exists(os.path.join(base_dir, vestibular, "GABARITOS")) and len(os.listdir(os.path.join(base_dir, vestibular, "GABARITOS"))) >= 1 for vestibular in vestibulares]
     status = ["done" if s else "loading" for s in status]
     urls = [vestibular.replace(" ", "-").lower()[4:] for vestibular in vestibulares]
     for vestibular, img, s, url in zip(vestibulares, imgs, status, urls):
