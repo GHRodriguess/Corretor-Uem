2fc0d464cb8e3c650df756799d2bf14aad9cfc9c
GHRodriguess <gabri.henri20088@gmail.com>
2025-02-16 09:25:11 -0300

    base_dir para prod

--- utils/global_utils.py
+++ utils/global_utils.py
@@ -2,7 +2,7 @@ import os
 
 def base_dir():
     # PARA USO INTERNO
-    base_dir = os.path.join("VESTIBULARES")
+    #base_dir = os.path.join("VESTIBULARES")
     # PARA PRODUÇÃO
-    #base_dir = os.path.join("Corretor-Uem","VESTIBULARES")
+    base_dir = os.path.join("Corretor-Uem","VESTIBULARES")
     return base_dir
