1c65999702756a44ceabad36c0c0c1b71bcd4c52
GHRodriguess <gabri.henri20088@gmail.com>
2025-08-15 20:57:55 -0300

    atualizando o cors para ver se deixa o site acessar

--- backend/api_corretor_uem/settings.py
+++ backend/api_corretor_uem/settings.py
@@ -65,6 +65,27 @@ CORS_ALLOWED_ORIGINS = [
     'https://corretoruem-git-react-ghrodriguess-projects.vercel.app',
 ]
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
 ROOT_URLCONF = 'api_corretor_uem.urls'
 
 TEMPLATES = [
