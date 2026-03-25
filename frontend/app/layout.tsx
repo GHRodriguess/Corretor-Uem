2ecfc86d37de62c4000e57f25c0cdc2f42e2cb18
gabrielrodrigues <gabri.henri20088@gmail.com>
2026-03-25 15:23:30 -0300

    adicionando o speedinsights da vercel

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 
@@ -21,6 +22,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html className="h-full">
+            <SpeedInsights />
             <body className="h-full">
                 {children}
             </body>
