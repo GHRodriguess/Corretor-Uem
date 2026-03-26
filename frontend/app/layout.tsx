2d5ce86ee7dbcb98d70e1905f4a8dd8608c3f6c1
gabrielrodrigues <gabri.henri20088@gmail.com>
2026-03-26 13:41:26 -0300

    adicionando analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 import "./globals.css";
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html className="h-full">
             <SpeedInsights />
+            <Analytics/>
             <body className="h-full">
                 {children}
             </body>
